7c15dfa01c24d252123cb7da19b30f845666a440
SweetySnail <shwogus1011@gmail.com>
2026-05-13 13:46:01 +0900

    Delete env value

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBi-R4m5tyc-FHpgOo2SXa9mMwOYXXt9b4",
-  authDomain: "roomescape-80d4d.firebaseapp.com",
-  projectId: "roomescape-80d4d",
-  storageBucket: "roomescape-80d4d.firebasestorage.app",
-  messagingSenderId: "717404686429",
-  appId: "1:717404686429:web:8ea8871a2f73332adb25b4",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
