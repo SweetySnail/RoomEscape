94d9bb8d2a4bfa1babe2f6f494f5c79396724aec
SweetySnail <shwogus1011@gmail.com>
2026-05-26 13:56:30 +0900

    change OAuth

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN_CUSTOM || process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
@@ -24,12 +24,12 @@ if (typeof window !== 'undefined') {
   }
 }
 
-// initializeAppCheck(app, {
-//   provider: new ReCaptchaV3Provider(
-//     process.env.REACT_APP_RECAPTCHA_SITE_KEY
-//   ),
-//   isTokenAutoRefreshEnabled: true,
-// });
+initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider(
+    process.env.REACT_APP_RECAPTCHA_SITE_KEY
+  ),
+  isTokenAutoRefreshEnabled: true,
+});
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
